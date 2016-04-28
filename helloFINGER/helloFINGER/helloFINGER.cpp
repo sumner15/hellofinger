a8ce783167cd792652631f9a580b2f211a164ca5
sumner15 <sumner15@localhost>
2016-04-28 15:19:33 -0700

    Working brainFINGER controller now switches between modes 3 and 4 without issue. Mode 3 now accepts actual position as desired position between movements and doesn't get angry when not starting from where it normally expects.

--- helloFINGER/helloFINGER/helloFINGER.cpp
+++ helloFINGER/helloFINGER/helloFINGER.cpp
@@ -13,11 +13,11 @@ double feedback1 = 0;		//initialize time-to-threshold feedback (use for GUI)
 double feedback2 = 0;		//initialize feedback for second finger
 
 // set parameters to connect to XPC target machine (initialize fingerbot)
-string ipAddress =	"129.101.53.73";		// UCI IP address   
-/* string ipAddress = "169.254.201.253";		// Wadsworth IP address */
+//string ipAddress =	"129.101.53.73";		// UCI IP address   
+string ipAddress = "169.254.201.253";		// Wadsworth IP address 
 string ipPort =		"22222";
 string modelName =	"brainFINGER";	
-/* string modelName =	"FingerEAERCtrl"; */
+//string modelName =	"FingerEAERCtrl"; 
 FingerBot finger = FingerBot(ipAddress, ipPort, modelName);
 
 // function to print updates to the console
@@ -47,7 +47,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	is because the trajectory planner takes the desired time as the desired 
 	movement completion time. Thus, the movement start time is ctt-moveDur.
 
-	All values are now in seconds! None of the parameters use milliseconds.
+	All time values are in seconds! 
 
 	full extension position is = 0. full flexion position is = 1.
 
@@ -75,7 +75,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	// sending FINGER movement parameters to the robot
     finger.setForcesOn(true);
-    finger.setKp(2.0, 2);
+    finger.setKp(3.0, 2);
     finger.setKd(0.2, 2);	
     finger.setTrajMode(trajMode);        
 	finger.setMovementDuration(moveDur);	
@@ -88,39 +88,41 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 
 
-	// create an example set of movements (flex mode 4, auto-extend mode 3)
+	/* create an example set of movements (flex mode 4, auto-extend mode 3) where 
+	the subject is expected to initiate the flexion/test phase. They will be auto
+	-returned to the starting extended position using a mode 3 movement */
 	for (int i=0; i<10; ++i){
-		nextPos = flexPos;
-		finger.setTrajMode(4.0); // movement detection
+		nextPos = flexPos;		
 		cout << "\n\n\n\n\n\n\n FLEX! \n\n" ;
+		finger.setTrajMode(4.0); // movement detection mode	
 		finger.setHitPos(nextPos,fingerToUse);    		
 		ctt = maxTD + finger.getTargetTime();     
-		finger.setHitTimes(ctt,fingerToUse); 		
-		feedback1 = finger.getFeedback(0);	
-		feedback2 = finger.getFeedback(1);	
+		finger.setHitTimes(ctt,fingerToUse); 				
 
 		// wait for movement to complete 
+		feedback1 = finger.getFeedback(0);	
+		feedback2 = finger.getFeedback(1);	
 		while(finger.getTargetTime() < (ctt + 0.5)){		
 			// print the new feedback (if subject exceeded the force threshold)
 			if (finger.getFeedback(0) != feedback1){
 				feedback1 = finger.getFeedback(0);	
-				cout << "FINGER 1 detected " << feedback1 << " sec" << endl;
+				cout << "FINGER 1 Force detect " << feedback1 << " sec" << endl;
 			}	
 			if (finger.getFeedback(1) != feedback2){
 				feedback2 = finger.getFeedback(1);
-				cout << "FINGER 2 detected " << feedback2 << " sec" << endl;
+				cout << "FINGER 2 Force detect " << feedback2 << " sec" << endl;
 			}
 		}
 		
 		// if either finger is partially flexed, return to extension
 		if ((finger.getPos1()>0.4) || (finger.getPos2()>0.4)){
 			nextPos = extendPos;
-			finger.setTrajMode(3.0); // auto movement
-			cout << "\n\n\n\n\n\n\n Auto EXTEND (relax) \n\n" ;					
+			cout << "\n\n\n\n\n\n\n Auto EXTEND (relax) \n\n" ;		
+			finger.setTrajMode(3.0); // auto movement						
 			finger.setHitPos(nextPos,fingerToUse);    		
 			ctt = moveDur + 0.1 + finger.getTargetTime();     
 			finger.setHitTimes(ctt,fingerToUse); 
-			Sleep(1000*moveDur+500);			
+			Sleep(1000*moveDur+500);	
 		} 			 					
 	} // end example movements for loop
       
